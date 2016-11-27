9b2c47f707919200f1a30da7cb76d386adf1b10c
aalen1 <aalen.s.d@gmail.com>
2016-11-27 04:12:52 +0300

    alyona push

---
+++ js/controllers.js
@@ -0,0 +1,59 @@
+angular.module('app.controllers', [])
+  
+.controller('131Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+
+.controller('page3Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('page12Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('page4Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('page5Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('page6Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ js/routes.js
@@ -0,0 +1,83 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('menu.131', {
+    url: '/page2',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/131.html',
+        controller: '131Ctrl'
+      }
+    }
+  })
+
+  .state('menu.page3', {
+    url: '/page3',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/page3.html',
+        controller: 'page3Ctrl'
+      }
+    }
+  })
+
+  .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'templates/menu.html',
+    controller: 'menuCtrl'
+  })
+
+  .state('menu.page12', {
+    url: '/page12',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/page12.html',
+        controller: 'page12Ctrl'
+      }
+    }
+  })
+
+  .state('menu.page4', {
+    url: '/page4',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/page4.html',
+        controller: 'page4Ctrl'
+      }
+    }
+  })
+
+  .state('menu.page5', {
+    url: '/page5',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/page5.html',
+        controller: 'page5Ctrl'
+      }
+    }
+  })
+
+  .state('menu.page6', {
+    url: '/page6',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/page6.html',
+        controller: 'page6Ctrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/side-menu21/page2')
+
+  
+
+});
